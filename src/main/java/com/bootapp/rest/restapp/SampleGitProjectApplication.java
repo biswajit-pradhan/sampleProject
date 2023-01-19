5342dc48496ae7f56e6f688c4da7f2979a787009
Biswajit Pradhan <pbiswajit967@gmail.com>
2023-01-19 10:32:17 +0530

    first commit

---
+++ src/main/java/com/bootapp/rest/restapp/SampleGitProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bootapp.rest.restapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleGitProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleGitProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bootapp/rest/restapp/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bootapp.rest.restapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/api/helloapi")
+	public String HelloApi() {
+		return "Hello World";
+	}
+}
